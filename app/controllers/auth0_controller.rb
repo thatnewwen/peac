b713bc0080bb487c558283c8ba516a48e2825455
surrealdetective <thefifth@gmail.com>
2018-02-12 09:45:13 -0600

    -try single sign on code throughout relevant controllers

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -4,8 +4,20 @@ class Auth0Controller < ApplicationController
     # and the IdP
     session[:userinfo] = request.env['omniauth.auth']
 
+    raise
+    secret = "JEdMKdLz%ZQ3w(bxRdk4K#NMrN/:p}6H9bF,Ej-F"
+    sso = SingleSignOn.parse(request.query_string, secret)
+    sso.email = 'suchwowalexau@gmail.com'#current_user.email # from devise
+    sso.name = 'Alex Au'#current_user.full_name # this is a custom method on the User class
+    sso.username = 'suchwowalexau' #current_user.email # from devise
+    sso.external_id = 1 #current_user.id # from devise
+    sso.sso_secret = secret
+
+    raise
+    redirect_to sso.to_url("http://discourse.local/session/sso_login")
+
     # Redirect to the URL you want after successful auth
-    redirect_to '/messages'
+    # redirect_to '/messages'
   end
 
   def failure
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,17 @@
 class MessagesController < ApplicationController
   include Secured
   def index
+    
+
+    secret = "JEdMKdLz%ZQ3w(bxRdk4K#NMrN/:p}6H9bF,Ej-F"
+    sso = SingleSignOn.parse(request.query_string, secret)
+    sso.email = 'suchwowalexau@gmail.com'#current_user.email # from devise
+    sso.name = 'Alex Au'#current_user.full_name # this is a custom method on the User class
+    sso.username = 'suchwowalexau' #current_user.email # from devise
+    sso.external_id = 1 #current_user.id # from devise
+    sso.sso_secret = secret
+
+    raise
+    redirect_to sso.to_url("http://discourse.local/session/sso_login")
   end
 end
