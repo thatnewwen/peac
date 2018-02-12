5d4738eac516346c3a6802beee7f36815e0ef0f6
surrealdetective <thefifth@gmail.com>
2018-02-12 10:32:45 -0600

    -fail on sso=SingleSignOn.parse

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -4,7 +4,6 @@ class Auth0Controller < ApplicationController
     # and the IdP
     session[:userinfo] = request.env['omniauth.auth']
 
-    raise
     secret = "JEdMKdLz%ZQ3w(bxRdk4K#NMrN/:p}6H9bF,Ej-F"
     sso = SingleSignOn.parse(request.query_string, secret)
     sso.email = 'suchwowalexau@gmail.com'#current_user.email # from devise
@@ -12,12 +11,10 @@ class Auth0Controller < ApplicationController
     sso.username = 'suchwowalexau' #current_user.email # from devise
     sso.external_id = 1 #current_user.id # from devise
     sso.sso_secret = secret
-
-    raise
     redirect_to sso.to_url("http://discourse.local/session/sso_login")
 
     # Redirect to the URL you want after successful auth
-    # redirect_to '/messages'
+    redirect_to '/messages'
   end
 
   def failure
