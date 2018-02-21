98ab3ee05b2a8c21801d7a161dcfafa22eb8e9f0
surrealdetective <thefifth@gmail.com>
2018-02-21 15:42:37 -0600

    abandon attempt to do sso w/ peac as centerpiece

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -4,17 +4,18 @@ class Auth0Controller < ApplicationController
     # and the IdP
     session[:userinfo] = request.env['omniauth.auth']
 
-    secret = "JEdMKdLz%ZQ3w(bxRdk4K#NMrN/:p}6H9bF,Ej-F"
-    sso = SingleSignOn.parse(request.query_string, secret)
-    sso.email = 'suchwowalexau@gmail.com'#current_user.email # from devise
-    sso.name = 'Alex Au'#current_user.full_name # this is a custom method on the User class
-    sso.username = 'suchwowalexau' #current_user.email # from devise
-    sso.external_id = 1 #current_user.id # from devise
-    sso.sso_secret = secret
-    redirect_to sso.to_url("http://discourse.local/session/sso_login")
-
+    # secret = "JEdMKdLz%ZQ3w(bxRdk4K#NMrN/:p}6H9bF,Ej-F"
+    # sso = SingleSignOn.parse(request.query_string, secret)
+    # sso.email = 'suchwowalexau@gmail.com'#current_user.email # from devise
+    # sso.name = 'Alex Au'#current_user.full_name # this is a custom method on the User class
+    # sso.username = 'suchwowalexau' #current_user.email # from devise
+    # sso.external_id = 1 #current_user.id # from devise
+    # sso.sso_secret = secret
+    # redirect_to sso.to_url("http://discourse.local/session/sso_login")
+    raise
+    redirect_to "http://discourse.local/session/sso_login?secret="
     # Redirect to the URL you want after successful auth
-    redirect_to '/messages'
+    # redirect_to '/messages'
   end
 
   def failure
